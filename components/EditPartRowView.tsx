1126dcfd484fd54c816720e1f3fb4ce0d1f6812d
Justin Abuyuan <abuyuanjustin@gmail.com>
2025-07-24 13:47:33 -0400

    Fixed refresh bug

--- components/EditPartRowView.tsx
+++ components/EditPartRowView.tsx
@@ -7,6 +7,8 @@ import {Label} from "@/components/ui/label";
 import {Button} from "@/components/ui/button";
 import {useInventoryContext} from "@/context/InventoryContext";
 import {Badge} from "@/components/ui/badge";
+import {useEffect, useState} from "react";
+import {ArrowRightIcon} from "@heroicons/react/16/solid";
 
 export default function EditPartRowView({partToBeEdited, canRenderEdits}: {
     partToBeEdited: InventoryPart,
@@ -15,17 +17,24 @@ export default function EditPartRowView({partToBeEdited, canRenderEdits}: {
     // Context
     const {currentInventory, setEditedInventory} = useInventoryContext();
 
-    // Helper methods
+    // Component state
+    const [originalPartToBeEdited, setOriginalPartToBeEdited] = useState<InventoryPart>();
+
+    useEffect(() => {
+        const origPart = currentInventory.find(partInInventory => partInInventory.sku === partToBeEdited.sku);
+        setOriginalPartToBeEdited(origPart);
+    }, [])
+
     /**
      * Handle deleting an item from the edit summary
      */
     const handleRevertPart = () => {
-        const origPart = currentInventory.find(partInInventory => partInInventory.sku === partInInventory.sku);
-        if (origPart) {
+        if (originalPartToBeEdited) {
+            console.log(`edited ${originalPartToBeEdited.name}: ${originalPartToBeEdited.quantity}\ncurrent ${partToBeEdited.name}: ${partToBeEdited.quantity}`);
             setEditedInventory(prev =>
                 prev.map(partInEditedInventory =>
                     partInEditedInventory.sku == partToBeEdited.sku ?
-                        {...partInEditedInventory, quantity: origPart.quantity} :  // revert to old value
+                        {...partInEditedInventory, quantity: originalPartToBeEdited.quantity} :  // revert to old value
                         partInEditedInventory  // otherwise leave alone and move on
                 )
             )
@@ -41,8 +50,16 @@ export default function EditPartRowView({partToBeEdited, canRenderEdits}: {
                 <Label>{partToBeEdited.name}</Label>
                 <Label className="text-sm text-gray-500 italic">{partToBeEdited.sku}</Label>
             </section>
-            <Badge className="size-6" variant="secondary">{partToBeEdited.quantity}</Badge>
-            <Button disabled={!canRenderEdits} variant="destructive" onClick={handleRevertPart}>Remove</Button>
+            <div className="flex flex-row items-center gap-4">
+                {originalPartToBeEdited && (
+                    <>
+                        <Badge className="size-6" variant="secondary">{originalPartToBeEdited.quantity}</Badge>
+                        <ArrowRightIcon className="size-4" />
+                    </>
+                )}
+                <Badge className="size-6" variant="default">{partToBeEdited.quantity}</Badge>
+            </div>
+            <Button disabled={!canRenderEdits} variant="destructive" onClick={handleRevertPart} type="button">Revert</Button>
         </div>
     )
 }
